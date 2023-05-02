6c6e6c98e8c5b99f32808a81b7c32dc0a80b2fa5
Arivera <alvaroriverafernandez@gmail.com>
2023-05-02 17:23:34 -0400

    Se agrega botón

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,15 +1,19 @@
 import styles from "../styles/card.module.css";
 import { Link } from "react-router-dom";
+import { DeleteFilled  } from '@ant-design/icons';
+
 export default function Card({ id, name, onClose, image, species, gender }) {
   return (
     <div className={styles.card}>
-      <button onClick={() => onClose(id)}>X</button>
+
    <Link to={`/detail/${id}`}>
       <h2>Nombre: {name}</h2>
       </Link>
       <h2>Especie: {species}</h2>
       <h2>Género: {gender}</h2>
       <img src={image} alt="" />
+
+      <button className={styles.button} onClick={() => onClose(id)}> <DeleteFilled /></button>
     </div>
   );
 }
